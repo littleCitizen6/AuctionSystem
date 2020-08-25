742992b6625052606a3fe1d35584ccd7106ef3dc
littleCitizen6 <thelittlecitizen6@gmail.com>
2020-08-25 20:28:10 +0300

    added products implements

---
+++ AuctionSystem/AuctionSystem/Products/Building.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuctionSystem.Products
+{
+    public class Building : IProduct
+    {
+        public Dictionary<string, string> Properties { get ; set ; }
+        public Building(List<double> roomsSize, Dictionary<string,string> prop)
+        {
+            Properties = prop;
+            Properties.Add("roomsCount", roomsSize)
+        }
+    }
+}
---
+++ AuctionSystem/AuctionSystem/Products/House.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuctionSystem.Products
+{
+    public class House : Building
+    {
+        public House(List<double> roomsSize, Dictionary<string, string> prop) : base(roomsSize, prop)
+        {
+        }
+    }
+}
