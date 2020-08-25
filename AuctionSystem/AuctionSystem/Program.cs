7fce65e1821e75f3af7382bf7fb3340af6fe3b0e
littleCitizen6 <thelittlecitizen6@gmail.com>
2020-08-25 19:40:46 +0300

    started proj

---
+++ AuctionSystem/AuctionSystem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AuctionSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
