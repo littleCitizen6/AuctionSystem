3ad362a2332089f0118223cb8b211659c28d8cee
littleCitizen6 <thelittlecitizen6@gmail.com>
2020-08-26 16:53:09 +0300

    added Agent proj

---
+++ AuctionSystem/Agents/Agent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using AuctionSystem.Common;
+using AuctionSystem.SellsManeger;
+
+namespace Agents
+{
+    public abstract class Agent : IBuyer
+    {
+        public string Name { get; set; }
+        public Agent(string name)
+        {
+            Name = name;
+        }
+
+        public abstract void IsIntresting(ISellManeger sellManeger);
+        public abstract void IsWantToRaise(ISellManeger sellManeger);
+    }
+}
