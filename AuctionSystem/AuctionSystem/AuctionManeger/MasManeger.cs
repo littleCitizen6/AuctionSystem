d772548228af6b181ae88b536de5325648f7d86c
citizen6 <theLittleCitizen6@gmail.com>
2020-08-26 20:11:07 +0300

    added start for execute

--- AuctionSystem/AuctionSystem/AuctionManeger/MasManeger.cs
+++ AuctionSystem/AuctionSystem/AuctionManeger/MasManeger.cs
@@ -48,6 +48,7 @@ namespace AuctionSystem.AuctionManeger
                             sell.StartSell();
                             });
                     });
+                    executer.Start();
                 }
             }
         }
