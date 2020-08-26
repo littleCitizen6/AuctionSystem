991c6e706752f6ae4bf0fe6011fe93e6f53a0b32
littleCitizen6 <thelittlecitizen6@gmail.com>
2020-08-26 17:07:54 +0300

    added percentageAgent

---
+++ AuctionSystem/Agents/PercentageAgent.cs
@@ -0,0 +1,34 @@
+﻿using AuctionSystem.SellsManeger;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Agents
+{
+    public class PercentageAgent : Agent
+    {
+        private int _percentage;
+        private Random _rander;
+        public PercentageAgent(string name, int precentage) : base(name)
+        {
+            _percentage = precentage;
+            _rander = new Random();
+        }
+
+        public override void IsIntresting(ISellManeger sellManeger)
+        {
+            if(_rander.Next(1,101)<=_percentage)
+            {
+                sellManeger.Subscribe(this);
+            }
+        }
+
+        public override void IsWantToRaise(ISellManeger sellManeger)
+        {
+            if (_rander.Next(1, 101) <= _percentage)
+            {
+                sellManeger.Offer(this, sellManeger.SellInfo.CurrentPrice + sellManeger.SellInfo.MinGrowth);
+            }
+        }
+    }
+}
