a631226c584dd9e2f406e8b7efc10b92d456ac69
citizen6 <theLittleCitizen6@gmail.com>
2020-08-27 22:40:01 +0300

    added GetFutureAuctions

--- AuctionSystem/MasMenu/MasController.cs
+++ AuctionSystem/MasMenu/MasController.cs
@@ -1,11 +1,32 @@
-﻿using System;
+﻿using AuctionSystem.AuctionManeger;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace MasMenu
 {
     public class MasController
     {
-        
+        IAuctionManeger _maneger;
+        public MasController(IAuctionManeger maneger)
+        {
+            _maneger = maneger;
+        }
+
+        public string GetFutureAuctions(string userInput)
+        {
+            StringBuilder builder = new StringBuilder();
+            _maneger.AllSells.Where(sell => sell.SellInfo.StartTime > DateTime.Now).ToList().ForEach(sell => 
+            {
+                builder.AppendLine($"product {sell.SellInfo.Product.Properties["name"]}, have start price of {sell.SellInfo.StartPrice}, and start at {sell.SellInfo.StartTime}");
+                sell.SellInfo.Product.Properties.Keys.ToList().ForEach(prop => 
+                {
+                    builder.AppendLine($"his prop {prop} is {sell.SellInfo.Product.Properties[prop]}");
+                });
+                builder.AppendLine();
+            });
+            return builder.ToString();
+        }
     }
 }
